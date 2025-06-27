d114427674ec8a50de8c9db393acae7a80fb81a0
DOUINEAU Romain <douineauromain@yahoo.fr>
2025-06-27 15:41:59 +0200

    use docs for build (test)

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ export default defineConfig(({ mode }) => {
       // Ajoutez cette ligne :
       base: '/sambarchivage2025/',
 
+      build: {
+        outDir: 'docs'
+      },
+
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
